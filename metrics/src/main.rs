14d15520dde7ce710f04e1f45e4e5d1f0e15758d
Asan Iusipov <a.yusipov@prisma-ai.com>
2022-11-02 19:36:20 +0200

    Add default value to common-labels to make this argument optional

--- metrics/src/main.rs
+++ metrics/src/main.rs
@@ -77,6 +77,9 @@ impl TypedValueParser for MapValueParser {
         _arg: Option<&clap::Arg>,
         value: &std::ffi::OsStr,
     ) -> Result<Self::Value, clap::Error> {
+        if(value.eq('')){
+            Ok(HashMap::new())
+        }
         let map: HashMap<String,String> = Regex::new(r",").unwrap().split(value.to_str().unwrap()).map(|x| key_value_parser(x).unwrap()).collect();
         Ok(map)
     }
@@ -106,7 +109,7 @@ pub(crate) struct Configuration {
     #[clap(name ="exclude-collectors", long = "exclude-collectors", env = "EXCLUDE_COLLECTORS", value_parser, value_delimiter(';'))]
     disabled_collectors: Vec<Collectors>,
 
-    #[clap(name ="common-labels", short = 'l', long = "common-labels", env = "COMMON_LABELS", value_parser = MapValueParser::new())]
+    #[clap(name ="common-labels", short = 'l', long = "common-labels", env = "COMMON_LABELS", value_parser = MapValueParser::new(), default_value = '')]
     common_labels: HashMap<String,String>,
 
     #[clap(name ="histogram-buckets", long = "histogram-buckets", env = "HISTOGRAM_BUCKETS", value_parser, value_delimiter(';'))]
